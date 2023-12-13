2e199e6728d49059c00258eb0fdcc1b41ccad3ea
LeeYejun1324 <106819072+LeeYejun1324@users.noreply.github.com>
2023-12-14 01:44:21 +0900

    Update simple_ImageSets.py

--- sfa/data_process/simple_ImageSets.py
+++ sfa/data_process/simple_ImageSets.py
@@ -11,7 +11,7 @@ train_len = (len(a)*70//100)
 train_set = a[:train_len]
 val_set = a[train_len:]
 
-root = 'C:\\Users\\dldpw\\Downloads\\084.강건한(Robust) 융합 센서 객체 인식 자율주행 데이터\\01.데이터\\1.Training\\NEW_DATASET\\training\\ImageSets\\'
+root = '../../dataset/kitti/ImageSets/'
 
 with open(root+'trainval.txt','w') as trainval:
     for idx, i in enumerate(origin):
